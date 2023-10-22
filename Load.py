0062d3543ef1cb5175656b759db4d016e41c22de
Po Yan <70246850+ChuanPoYan@users.noreply.github.com>
2023-10-22 12:45:15 +0800

    Allow easier querying of full transaction data

--- Load.py
+++ Load.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
 full_df = pd.DataFrame()
-for i in range (88):
-    df = pd.read_csv(f'transactions_{i}.csv')
-    full_df = pd.concat([full_df,df])
-
-
-full_df.to_csv("full_transactions.csv")
+i = 0
+while True:
+    try:
+        df = pd.read_csv(f'data/queried/transactions_{i}.csv')
+        full_df = pd.concat([full_df,df])
+        i += 1
+    except:
+        full_df.to_csv('data/queried/full_transactions.csv', index=False)
+        break
